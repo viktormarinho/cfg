c28f4b5a89a07392a17ece5d8591c5028d61d381
viktormarinho <56888067+viktormarinho@users.noreply.github.com>
2025-07-31 00:37:28 -0300

    decent

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,3 +1,4 @@
+
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
@@ -12,6 +13,41 @@ config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
 config.font_size = 20
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+config.use_dead_keys = false
+
+config.keys = {
+  {
+    key = "t",
+    mods = "CMD|SHIFT",
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    key = 'k',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.ActivatePaneDirection 'Next',
+  },
+  {
+    key = 'j',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.ActivatePaneDirection 'Prev',
+  },
+}
 
 -- Finally, return the configuration to wezterm:
 return config
