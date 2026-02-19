f935383a97119dad2ec624c3c6716c91373ee5df
viktormarinho <56888067+viktormarinho@users.noreply.github.com>
2026-02-19 10:19:36 -0300

    Configure background opacity and inactive pane settings
    Added opacity settings for window and text backgrounds, and adjusted inactive pane HSB values.

--- .wezterm.lua
+++ .wezterm.lua
@@ -17,6 +17,9 @@ config.font_size = 20
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
+config.window_background_opacity = 0.7
+config.text_background_opacity = 0.85
+
 config.window_padding = {
   left = 0,
   right = 0,
@@ -47,6 +50,11 @@ config.keys = {
     mods = 'CMD|SHIFT',
     action = wezterm.action.ActivatePaneDirection 'Prev',
   },
+}
+
+config.inactive_pane_hsb = {
+  saturation = 0.9,
+  brightness = 0.6,
 }
 
 -- Finally, return the configuration to wezterm:
